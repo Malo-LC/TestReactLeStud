4f15a1a43a185c2b9d8c48a791fc42dd2ed77d5b
Malo-LC <malolc2001@gmail.com>
2022-08-16 14:02:39 +0200

    commentaires

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,14 @@ function App() {
   const [search, setSearch] = useState('');
   useEffect(() => {
     async function getPixabayQuery() {
+      // get les photos avec async/await
       const res = await axios.get(
-        `https://pixabay.com/api/?key=29294609-f445397148cc5be28fc78b375&q=${search}&image_type=photo&pretty=true`
+        `https://pixabay.com/api/?key=29294609-f445397148cc5be28fc78b375&q=${search}&image_type=photo&pretty=true` // Je laisse ma clé de l'api car c'est gratuit
       );
       setData(res.data.hits);
     }
     getPixabayQuery();
-  }, [search]);
+  }, [search]); // on execute dès que on cherche qlqchose dans la barre
 
   return (
     <div className="w-screen grid justify-items-center">
@@ -21,14 +22,15 @@ function App() {
           Recherchez une image
         </h1>
         <input
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)} // on met à jour la recherche
           placeholder="Chercher..."
           className="form-control px-3 py-1.5 text-gray-700 bg-clip-padding border border-solid 
           border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:border-blue-600 focus:outline-none"
         />
-        {data.length !== 0 ? (
+        {data.length !== 0 ? ( // si il y a des photos à afficher
           <div className="flex flex-column justify-center border rounded flex-wrap w-10/12 m-2">
             {data.map((photo) => {
+              // on les affiche toutes
               return (
                 <a href={photo.pageURL} target="_blank" rel="noreferrer">
                   <img
@@ -42,7 +44,8 @@ function App() {
             })}
           </div>
         ) : (
-          <h1>Chargment</h1>
+          // sinon on affiche rien
+          <h1>Aucune photo...</h1>
         )}
       </div>
     </div>
